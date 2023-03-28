20005cee8acebfb84796b7bb986594fbf4eff8dc
imMatheus <matheusmendesbarata03@gmail.com>
2023-03-28 13:02:27 +0200

    added layout component and started on playlist page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,5 @@
-import Sidebar from '@/components/Sidebar'
-import React from 'react'
+import Layout from '@/components/Layout'
 
 export default function index() {
-    return (
-        <div className='flex h-screen flex-col'>
-            <div className='flex flex-1 overflow-y-auto'>
-                <Sidebar />
-                <main className='flex-1'>main</main>
-            </div>
-            <footer className='h-[59px]'>player</footer>
-        </div>
-    )
+    return <Layout>hej</Layout>
 }
